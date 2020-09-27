d0161306f1ccb22a7d2980835ec4e88b3478fd7a
Vladimir Vozniak <vozniak.vladimir90@gmail.com>
2020-09-27 15:13:04 +0300

    Add new file - ShakerSortArray and BubbleSortArray was modified

--- BubbleSortArray.java
+++ BubbleSortArray.java
@@ -4,7 +4,7 @@ public class BubbleSortArray {
     public static void bubbleSort(int[] array) {
         int temp;
         for(int i = 0; i < array.length; i++) {
-            for (int j = i + 1; j < array.length - 1; j++) {
+            for (int j = i + 1; j < array.length; j++) {
                 if(array[i] > array[j]) {
                     temp = array[i];
                     array[i] = array[j];
---
+++ ShakerSortArray.java
@@ -0,0 +1,39 @@
+public class ShakerSortArray {
+    public static int[] array = {7, 2, 3, 15, 5, 0, 8, 1, 9, 99, 84, 56, 71, 63, 48};
+
+    public static void sort(int[] array) {
+        int left = 0;
+        int right = array.length - 1;
+        int temp;
+        do {
+            for (int i = left; i < array.length - 1; i++) {
+                if (array[i] > array[i + 1]) {
+                    temp = array[i];
+                    array[i] = array[i + 1];
+                    array[i + 1] = temp;
+                }
+            }
+            left++;
+            for (int i = right; i > 0; i--) {
+                if (array[i] < array[i - 1]) {
+                    temp = array[i];
+                    array[i] = array[i - 1];
+                    array[i - 1] = temp;
+                }
+            }
+            right--;
+        } while (left <= right);
+        printArray(array);
+    }
+
+    public static void printArray(int[] array) {
+        for (Integer i : array) System.out.print(i + " ");
+
+        System.out.println();
+        System.out.println("Array is sorted!");
+    }
+
+    public static void main(String[] args) {
+        sort(array);
+    }
+}
